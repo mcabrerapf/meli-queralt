54b7fda66fcb385dfda9932466a936f4139b1462
Manuel <mcabrerapf@gmail.com>
2017-01-14 18:17:08 +0100

    nav done

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+console.log("runnnnningggS")
+function myFunction() {
+ 	var x = document.getElementById("myTopnav");
+ 	if (x.className === "topnav") {
+ 		x.className += " responsive";
+ 	} else {
+ 		x.className = "topnav";
+ 	}
+ 	console.log("hello")
+ }
+ console.log("yess")
