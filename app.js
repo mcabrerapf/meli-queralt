30700e84b7c079c9720857d4991bc0eed6ca7012
Manuel <mcabrerapf@gmail.com>
2017-01-22 18:14:26 +0100

    v1.2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = procces.env.PORT ||3000; 	
+const PORT = proccess.env.PORT || 3000; 	
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 
